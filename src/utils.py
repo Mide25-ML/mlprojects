b810162550a6f3db40238bcec5d72cf1c097dc25
Mide25-ML <tibson1@gmail.com>
2025-06-23 21:51:09 +0100

    Prediction

--- src/utils.py
+++ src/utils.py
@@ -37,7 +37,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            model.fit(X_train,y_train) 
             #model.fit(X_train, y_train) #Train Model
 
             y_train_pred = model.predict(X_train)
@@ -52,5 +52,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e,sys)
